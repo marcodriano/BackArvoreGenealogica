4da8abb4e0a81c09b673f195c7d6be70a478b2e6
marcodriano <marcosadr@outlook.com>
2025-02-12 12:04:01 -0300

    adicionando relações

--- src/models/Member.js
+++ src/models/Member.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const memberSchema = new mongoose.Schema({
   name: { type: String, required: true },
   birthDate: { type: Date },
-  relationship: { type: String, required: true },
+  relationship: { type: String, required:true, enum: ['pai', 'mãe', 'irmã', 'irmão', 'avó', 'avô', 'tio', 'tia', 'primo', 'prima', 'sobrinho', 'neto'], default: 'biológica' },
   relationshipType: { type: String, enum: ['biológica', 'adotiva', 'meio-irmã', 'meio-irmão'], default: 'biológica' },
   parentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Member' }, // Referência ao pai/mãe
 });
